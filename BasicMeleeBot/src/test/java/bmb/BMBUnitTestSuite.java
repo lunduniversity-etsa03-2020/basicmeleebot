6d1c711dc91fa131d529d52010b88c19f2da055c
Markus Borg <markus.borg@ri.se>
2020-02-21 14:48:28 +0100

    Changed packages

--- BasicMeleeBot/src/test/java/bmb/BMBUnitTestSuite.java
+++ BasicMeleeBot/src/test/java/bmb/BMBUnitTestSuite.java
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-package se.lth.cs.etsa02.basicmeleebot.test;
+package bmb.test;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
